f35ebecbfb4da96eae3f137e6fca9fad925ddef7
alex <amartinezgonzalez.cf@iesesteveterradas.cat>
2024-04-22 19:43:37 +0200

    .

--- Api/src/api/models/word.js
+++ Api/src/api/models/word.js
@@ -5,7 +5,7 @@ const wordSchema = new mongoose.Schema({
   idioma: { type: String, required: true },
   palabra: { type: String, required: true },
   veces_utilizadas: { type: Number, required: true }
-},{ collection: 'Diccionario' });
+},{ collection: 'diccionarios' });
 
 // Crea el modelo basado en el esquema
 const Word = mongoose.model('Word', wordSchema);
