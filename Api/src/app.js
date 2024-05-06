dd223dcc019726b79c02950615a2896a5db3d21f
alex <amartinezgonzalez.cf@iesesteveterradas.cat>
2024-05-06 17:43:57 +0200

    .

--- Api/src/app.js
+++ Api/src/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/db');
 const { v4: uuidv4 } = require('uuid');
-const userRoutes = require('./api/routes/userRoutes');
 const crypto = require('crypto');
-const Event = require('./api/models/event');
+const Action = require('./api/models/action'); 
 const User = require('./api/models/usuari');
+const Game = require('./api/models/partida');
 const app = express();
 const Wordd = require('./api/models/word');
+
 
 app.use(express.json());
 app.set('json spaces', 2);
@@ -16,36 +17,20 @@ function generateApiKey(length = 64) {
   return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
 }
 
+function generateRandomName() {
+  const words = ["Eclipse", "Nova", "Orbit", "Pulsar", "Quasar", "Stellar", "Nebula", "Galaxy", "Meteor", "Cosmos", "Vortex", "Comet", "Asteroid", "Celestial", "Gravity", "Blackhole", "Starlight", "Moonlight", "Sunburst", "Horizon"];
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const randomNumber = Math.floor(Math.random() * 10000);
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const randomLetter = letters[Math.floor(Math.random() * letters.length)];
+  return `${words[randomIndex]}-${randomNumber}${randomLetter}`;
+}
+
+
 mongoose.connect(dbConfig.MONGODB_URI).then(() => console.log("Connectat a MongoDB"))
   .catch(err => console.error("No s'ha pogut connectar a MongoDB", err));
 
-app.get('/api/health', (req, res) => {
-  res.json({ status: "OK" });
-});
 
-app.use('/api', userRoutes);
-
-app.post('/api/events', async (req, res) => {
-  try {
-    const event = new Event(req.body);
-    await event.save();
-    res.status(201).send(event);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
-});
-
-app.get('/api/events/:id', async (req, res) => {
-  try {
-    const event = await Event.findById(req.params.id);
-    if (!event) {
-      return res.status(404).send("L'esdeveniment no s'ha trobat.");
-    }
-    res.send(event);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-});
 
 app.post('/api/user/register', async (req, res) => {
   if (!req.body.email || !req.body.nickname) {
@@ -67,6 +52,31 @@ app.post('/api/user/register', async (req, res) => {
   } catch (err) {
     console.error("Error while registering user:", err);
     res.status(400).send(err.message);
+  }
+});
+
+// Endpoint para obtener todos los idiomas disponibles
+app.get('/api/languages', async (req, res) => {
+  try {
+    const languages = await Wordd.distinct('idioma');
+    res.json(languages);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+// Endpoint para obtener palabras por idioma y letra inicial
+app.get('/api/words/:idioma/:letter', async (req, res) => {
+  try {
+    const regex = new RegExp(`^${req.params.letter}`, 'i'); // Expresión regular para hacer la búsqueda insensible a mayúsculas
+    const words = await Wordd.find({ 
+      idioma: req.params.idioma,
+      palabra: { $regex: regex }
+    });
+    res.json(words.map(word => word.palabra));  // Asegúrate de retornar el campo 'palabra'
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
@@ -100,30 +110,256 @@ app.post('/api/user/update', async (req, res) => {
   }
 });
 
-// Endpoint para obtener todos los idiomas disponibles
-app.get('/api/languages', async (req, res) => {
+// Endpoint para crear una nueva partida
+app.post('/api/games', async (req, res) => {
   try {
-    const languages = await Wordd.distinct('idioma');
-    res.json(languages);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-
-// Endpoint para obtener palabras por idioma y letra inicial
-app.get('/api/words/:idioma/:letter', async (req, res) => {
-  try {
-    const regex = new RegExp(`^${req.params.letter}`, 'i'); // Expresión regular para hacer la búsqueda insensible a mayúsculas
-    const words = await Wordd.find({ 
-      idioma: req.params.idioma,
-      palabra: { $regex: regex }
+    const offset = 2 * 60 * 60 * 1000; // 2 horas convertidas a milisegundos
+    const fechaInicio = new Date(new Date().getTime() + offset);
+    const newGame = new Game({
+      _id: uuidv4(), // Genera un UUID para la partida
+      nombrePartida: generateRandomName(), // Nombre de partida generado aleatoriamente
+      fechaInicio: fechaInicio, // Fecha actual como fecha de inicio
+      fechaFin: null, // Fecha de fin es null inicialmente
+      tipo: "multijugador", // Tipo fijo como 'multijugador'
+      letrasDelRosco: req.body.letrasDelRosco, // Letras del rosco enviadas en la solicitud
+      jugadores: [] // Inicializa la lista de jugadores vacía
     });
-    res.json(words.map(word => word.palabra));  // Asegúrate de retornar el campo 'palabra'
+
+    // Guardar la partida en la base de datos
+    await newGame.save();
+
+    // Enviar respuesta de éxito
+    res.status(201).json(newGame);
   } catch (error) {
-    res.status(500).send(error.message);
+    // Enviar respuesta de error
+    console.error("Error al crear la partida:", error);
+    res.status(400).json({ message: error.message });
   }
 });
+
+
+// Endpoint para añadir un jugador a una partida existente utilizando apiKey
+app.post('/api/games/:id/join', async (req, res) => {
+  try {
+    const apiKey = req.headers['x-api-key']; // Recupera el apiKey desde el header
+    if (!apiKey) {
+      return res.status(401).send("ApiKey no proporcionado.");
+    }
+
+    // Verifica que el apiKey corresponda a un usuario registrado
+    const user = await User.findOne({ api_key: apiKey });
+    if (!user) {
+      return res.status(401).send("ApiKey inválido o usuario no encontrado.");
+    }
+
+    // Busca la partida por ID para recuperar el nombre y actualizar el documento
+    const game = await Game.findById(req.params.id);
+    if (!game) {
+      return res.status(404).send("Juego no encontrado.");
+    }
+
+    // Si el usuario es válido, procede a añadirlo a la partida
+    const gameUpdate = {
+      $push: {
+        jugadores: {
+          uuidJugador: user.uuid, // Utiliza el uuid del usuario encontrado
+          nickname: user.nickname,
+          puntuacionTotal: 0,  // Inicializa la puntuación y otros campos según sea necesario
+          palabrasPuntuadas: []
+        }
+      }
+    };
+
+    // Busca por ID y actualiza el documento correspondiente
+    const updatedGame = await Game.findByIdAndUpdate(req.params.id, gameUpdate, { new: true });
+
+    if (!updatedGame) {
+      return res.status(404).send("Juego no encontrado.");
+    }
+
+    // Crea y guarda la acción de unirse a la partida
+    const joinAction = new Action({
+      tipoAccion: "jugador_unido",
+      nickname: user.nickname,
+      uuidJugador: user.uuid,
+      idPartida: req.params.id,
+      nombrePartida: game.nombrePartida, 
+      tiempoAccion: new Date() // Esto registrará el tiempo exacto en formato ISODate automáticamente
+    });
+
+    await joinAction.save();
+
+    // Responde al cliente con la partida actualizada
+    res.status(200).json(updatedGame);
+  } catch (error) {
+    console.error("Error al unir jugador a la partida:", error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.post('/api/update-word-score', async (req, res) => {
+  try {
+    const { apiKey, gameId, palabra, puntuacion } = req.body;
+
+    if (!apiKey || !gameId || !palabra || puntuacion == null) {
+        return res.status(400).send("Faltan campos necesarios: apiKey, gameId, palabra, o puntuacion.");
+    }
+
+    const user = await User.findOne({ api_key: apiKey });
+    if (!user) {
+        return res.status(401).send("API key inválida o usuario no encontrado.");
+    }
+
+    const updateOperations = {
+        $push: {
+            "jugadores.$.palabrasPuntuadas": {
+                palabra,
+                puntuacion
+            }
+        },
+        $inc: {
+            "jugadores.$.puntuacionTotal": puntuacion // Incrementa la puntuación total
+        }
+    };
+
+    const updatedGame = await Game.findOneAndUpdate(
+        { _id: gameId, "jugadores.uuidJugador": user.uuid },
+        updateOperations,
+        { new: true }
+    );
+
+    if (!updatedGame) {
+        console.log("No se encontró la partida o el jugador para la actualización.");
+        return res.status(404).send("Partida o jugador no encontrado.");
+    }
+
+    // Encuentra el nombre de la partida por idPartida
+    const partida = await Game.findById(gameId);
+    if (!partida) {
+      return res.status(404).send("Partida no encontrada.");
+    }
+    const offset = 2 * 60 * 60 * 1000; // 2 horas convertidas a milisegundos
+    const fechaInicio = new Date(new Date().getTime() + offset);
+    // Crea la acción de palabra guardada
+    const newAction = new Action({
+      tipoAccion: 'palabra_guardada',
+      uuidJugador: user.uuid, 
+      idPartida: gameId,
+      nombrePartida: partida.nombrePartida,
+      tiempoAccion: fechaInicio,
+      palabraPuntuacion: {
+        palabra: palabra,
+        puntuacion: puntuacion
+      }
+    });
+
+    await newAction.save();
+
+    console.log("Actualización exitosa:", updatedGame);
+    res.status(200).json(updatedGame);
+  } catch (error) {
+    console.error("Error al actualizar palabras puntuadas:", error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+app.post('/api/words/check', async (req, res) => {
+  try {
+    const { palabra, idioma, api_key, idPartida } = req.body;
+
+    if (!palabra || !idioma || !api_key || !idPartida) {
+      return res.status(400).send("Es necesario proporcionar la palabra, idioma, api_key y idPartida.");
+    }
+
+    // Encuentra al usuario mediante api_key
+    const user = await User.findOne({ api_key: api_key });
+    if (!user) {
+      return res.status(401).send("Usuario no encontrado o api_key inválida.");
+    }
+
+    // Encuentra el nombre de la partida por idPartida
+    const partida = await Game.findById(idPartida);
+    if (!partida) {
+      return res.status(404).send("Partida no encontrada.");
+    }
+
+    // Busca la palabra en la base de datos
+    const word = await Wordd.findOne({ palabra: palabra, idioma: idioma });
+
+    if (word) {
+      // Si la palabra existe, actualiza el contador de veces utilizadas
+      word.veces_utilizadas += 1;
+      await word.save();
+      return res.status(200).json({
+        exists: true,
+        message: "La palabra existe en el diccionario y la acción ha sido guardada.",
+        palabra: word.palabra,
+        idioma: word.idioma,
+        veces_utilizadas: word.veces_utilizadas
+      });
+    }
+
+    const offset = 2 * 60 * 60 * 1000; // 2 horas convertidas a milisegundos
+    const fechaInicio = new Date(new Date().getTime() + offset);
+    // Crea la acción de palabra inválida
+    const newInvalidAction = new Action({
+      tipoAccion: 'palabra_invalida',
+      uuidJugador: user.uuid,
+      idPartida: idPartida,
+      nombrePartida: partida.nombrePartida,
+      tiempoAccion: fechaInicio,
+      detalleInvalidacion: {
+        palabraIntentada: palabra,
+        razon: 'No existe en el diccionario'
+      }
+    });
+
+    // Guarda la acción en la base de datos
+    await newInvalidAction.save();
+    return res.status(200).json({
+      exists: false,
+      message: "La palabra no existe en el diccionario."
+    });
+
+  } catch (error) {
+    console.error("Error al verificar la palabra:", error);
+    res.status(500).json({ message: "Error del servidor al procesar la solicitud." });
+  }
+});
+
+
+
+
+// Endpoint para actualizar la fecha de finalización de una partida
+app.patch('/api/games/:id/finish', async (req, res) => {
+  try {
+    const offset = 2 * 60 * 60 * 1000; // 2 horas convertidas a milisegundos
+    const fechaFin = new Date(new Date().getTime() + offset);
+    const gameId = req.params.id;
+
+    // Configura la fecha de finalización con la fecha y hora actual del sistema
+    const gameUpdate = {
+      fechaFin: fechaFin  // Guarda la fecha y hora actual
+    };
+
+    // Actualiza el juego especificado por ID con la nueva fecha de finalización
+    const updatedGame = await Game.findByIdAndUpdate(gameId, gameUpdate, { new: true });
+
+    if (!updatedGame) {
+      return res.status(404).send("Juego no encontrado.");
+    }
+
+    // Devuelve el juego actualizado
+    res.status(200).json(updatedGame);
+  } catch (error) {
+    console.error("Error al actualizar la fecha de finalización del juego:", error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+
 
 
 module.exports = app;
--- Api/src/server.js
+++ Api/src/server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 const http = require('http');
 const { Server } = require('socket.io');
+const axios = require('axios');
 let letras = null;
+let partidaId = null;
+let cont = 0;
+
 
 const server = http.createServer(app);
 const io = new Server(server);
@@ -18,6 +22,7 @@ class Joc {
     this.properInici = Date.now() + this.partidaDuracio;
     this.enPartida = false;
     this.ciclarJoc();
+    this.iniciarPartida();
   }
 
   ciclarJoc() {
@@ -25,26 +30,56 @@ class Joc {
       this.enPartida = !this.enPartida;
       const nextDuration = this.enPartida ? this.partidaDuracio : this.pausaDuracio;
       this.properInici = Date.now() + nextDuration;
-      if (this.enPartida) {
-        letras = this.generarLetrasAleatorias();
+      if(this.enPartida){
+        console.log("\nPARTIDA INICIADA\n");
         io.emit('PARTIDA_INICIADA', { message: '\n¡Una nueva partida ha comenzado!', enPartida: this.enPartida ,letras : letras});
+
+      }else if (!this.enPartida){
+        this.actualizarFechaFinPartida(partidaId);
+        this.iniciarPartida();
       }
       this.ciclarJoc();
     }, this.enPartida ? this.partidaDuracio : this.pausaDuracio);
   }
 
-  consultaTempsRestant() {
-    const tempsRestant = this.properInici - Date.now();
-    return { tempsRestant, enPartida: this.enPartida };
+  async iniciarPartida() {
+    try {
+      letras = this.generarLetrasAleatorias();
+      const response = await axios.post('https://roscodrom6.ieti.site/api/games', {
+        letrasDelRosco: letras
+      });
+      console.log('Partida creada:', response.data);
+      partidaId = response.data._id;
+      return partidaId;
+    } catch (error) {
+      console.error('Error al crear la partida:', error);
+    }
+  }
+
+  async actualizarFechaFinPartida(partidaId) {
+    const url = `https://roscodrom6.ieti.site/api/games/${partidaId}/finish`;
+  
+    try {
+      const response = await axios.patch(url);
+      console.log('Partida actualizada con fecha de finalización:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al actualizar la fecha de finalización de la partida:', error);
+      return null;
+    }
   }
 
   generarLetrasAleatorias() {
     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','X', 'Y', 'Z'];
     return letters.sort(() => 0.5 - Math.random()).slice(0, 7).map(letra => ({ letra }));
   }
+
+
+  consultaTempsRestant() {
+    const tempsRestant = this.properInici - Date.now();
+    return { tempsRestant, enPartida: this.enPartida };
+  }
 }
-
-
 
 
 
@@ -59,41 +94,68 @@ function calcularPuntuacio(paraula) {
   return puntuacio;
 }
 
-const joc = new Joc(20000, 20000);
+const joc = new Joc(60000, 60000);
 
 io.on('connection', (socket) => {
   console.log('Usuari connectat');
 
-  const respostaInmediata = joc.consultaTempsRestant();
-  socket.emit('TEMPS_PER_INICI', respostaInmediata);
+  
   
   const intervalId = setInterval(() => {
     const resposta = joc.consultaTempsRestant();
-    socket.emit('TEMPS_PER_INICI', resposta);
-  }, 10000);
+    io.emit('TEMPS_PER_INICI', resposta);
+  }, 1000);
 
   socket.on('TEMPS_PER_INICI', () => {
     const resposta = joc.consultaTempsRestant();
     socket.emit('TEMPS_PER_INICI', resposta);
   });
 
-  socket.on('ALTA', (data) => {
-
-    console.log(`Petició d'alta rebuda amb dades: ${data}`);
-    socket.emit('ALTA_CONFIRMADA', {message: 'Alta processada correctament', data });
-  });
-
-  socket.on('PARAULA', (data) => {
-    if (joc.enPartida ){
-      console.log(`Palabra: ${data.paraula}, API_KEY: ${data.API_KEY}`);
-      if (data.paraula) {
-          let puntuacion = calcularPuntuacio(data.paraula);
-          socket.emit('Puntuacion', { message: `Puntuación de la palabra '${data.paraula}': ${puntuacion}`, data });
-      }
-    }else{
-      socket.emit('Puntuacion',{ message: `No estamos en partida`, data })
+  socket.on('ALTA', async (data) => {
+    if (!joc.enPartida) {  
+      const result = await unirJugadorAPartida(data, partidaId);
+      socket.emit('ALTA_CONFIRMADA', result);
+    } else {
+      socket.emit('ALTA_CONFIRMADA', { message: 'La partida ya ha empezado'});
     }
-    
+  });
+  
+
+
+  socket.on('PARAULA', async (data) => {
+    if (joc.enPartida) {
+      console.log(`Palabra: ${data.palabra}, API_KEY: ${data.apiKey}`);
+
+      if (data.palabra && data.apiKey) {
+        const result = await mirarPalabra(data, partidaId);
+      if (result.exists) {
+        let puntuacion = calcularPuntuacio(data.palabra);
+        io.emit('Puntuacion', {
+          message: `Puntuación de la palabra '${data.palabra}': ${puntuacion}`,
+          palabra: data.palabra,
+          puntuacion: puntuacion,
+          existe: true
+      });
+        const updateResult = await actualizarPuntuacionPalabra(data.apiKey, partidaId, data.palabra, puntuacion);
+        if (updateResult.success) {
+          console.log(`Puntuación de la palabra '${data.palabra}': ${puntuacion} actualizada correctamente en la base de datos.`);
+        } else {
+          console.log(`Error al actualizar la puntuación de la palabra '${data.palabra}': ${updateResult.message}`);
+        }
+
+        
+      } else {
+        socket.emit('Puntuacion', {
+            message: result.message,
+            existe: false
+        });
+      }
+      } else {
+          socket.emit('Puntuacion', { message: "No se proporcionó una palabra válida.", data });
+      }
+    } else {
+      socket.emit('Puntuacion', { message: `No estamos en partida`, data });
+    }
 });
 
   socket.onAny((event, ...args) => {
@@ -109,6 +171,89 @@ io.on('connection', (socket) => {
     clearInterval(intervalId);
   });
 });
+
+async function unirJugadorAPartida(data, partidaId) {
+  try {
+      // Verifica que la data incluya la API key y el ID de la partida
+      if (!data.apiKey) {
+          console.error('API key no proporcionada');
+          return { message: 'API key no proporcionada' };
+      }
+      if (!partidaId) {
+          console.error('ID de la partida no proporcionado');
+          return { message: 'ID de la partida no proporcionado' };
+      }
+
+      // Configura la URL del endpoint dinámicamente con el ID de la partida
+      const url = `https://roscodrom6.ieti.site/api/games/${partidaId}/join`;  // Usa el parámetro `id` aquí
+
+      // Configura las opciones para la solicitud de Axios
+      const config = {
+          headers: {
+              'x-api-key': data.apiKey // Usa la API key proporcionada por el jugador
+          }
+      };
+
+      const response = await axios.post(url, {}, config);
+      console.log('Jugador unido a la partida:', response.data);
+      return { message: 'Jugador unido a la partida correctamente', data: response.data };
+  } catch (error) {
+      console.error('Error al unir jugador a la partida:', error);
+      return { message: 'Error al unir jugador a la partida', error: error.message };
+  }
+}
+
+
+async function mirarPalabra(data, partidaId) {
+  try {
+    // Verifica que la data incluya la API key y el ID de la partida
+    if (!data.apiKey) {
+        console.error('API key no proporcionada');
+        return { message: 'API key no proporcionada' };
+    }
+    if (!partidaId) {
+        console.error('ID de la partida no proporcionado');
+        return { message: 'ID de la partida no proporcionado' };
+    }
+
+    const url = `https://roscodrom6.ieti.site/api/words/check`;
+    const response = await axios.post(url, {
+      palabra: data.palabra,
+      idioma: "catalan",
+      api_key: data.apiKey,
+      idPartida: partidaId
+    });
+
+    if (response.data && response.data.exists) {
+      return { exists: true, message: 'Palabra encontrada y contabilizada' };
+    } else {
+      return { exists: false, message: 'Palabra no encontrada en el diccionario' };
+    }
+  } catch (error) {
+    console.error('Error al verificar la palabra:', error);
+    return { exists: false, message: 'Error al verificar la palabra', error: error.message };
+  }
+}
+
+async function actualizarPuntuacionPalabra(apiKey, gameId, palabra, puntuacion) {
+  try {
+    const url = 'https://roscodrom6.ieti.site/api/update-word-score';
+    const body = {
+      apiKey: apiKey,
+      gameId: gameId,
+      palabra: palabra,
+      puntuacion: puntuacion
+    };
+    const response = await axios.post(url, body);
+    return { success: true, data: response.data };
+  } catch (error) {
+    console.error('Error al actualizar la puntuación de la palabra:', error);
+    return { success: false, message: error.message };
+  }
+}
+
+
+
 
 const port = process.env.PORT || 80;
 server.listen(port, () => console.log(`Escoltant en el port ${port}...`));
